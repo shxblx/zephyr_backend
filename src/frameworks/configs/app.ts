8dbdec20a1fc2e245075a979f0b46308f90dd8f1
shxblx <shiblibasheer27@gmail.com>
2024-06-28 22:53:38 +0530

    backend structure done

---
+++ src/frameworks/configs/app.ts
@@ -0,0 +1,12 @@
+import express, { Express } from "express";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
