c2ad7c8eab0098f69671d3a8b5f0210dac6f5641
shxblx <shiblibasheer27@gmail.com>
2024-08-24 17:03:43 +0530

    fix : cookie not showing

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -77,6 +77,7 @@ class UserController {
           httpOnly: true,
           secure: process.env.NODE_ENV !== "development",
           maxAge: 30 * 24 * 60 * 60 * 1000,
+          sameSite: "lax"
         });
 
         return res.status(loginVerified.status).json(loginVerified.data);
